b56a7364846939b05e1cf6e7b92768d275a0401f
ehddnr <ehddnrdlaos@gmail.com>
2023-08-05 23:53:45 +0900

    Update/0805

--- DynamicQueue/main.cpp
+++ DynamicQueue/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include "queue.h"
+enum Command
+{
+	DELETEALL = 0,
+	ENQUEUE = 1,
+	DEQUEUE = 2,
+	QUIT = 3
+};
 
 int main()
 {
+	Queue myQueue;
+
 	
+	int command{};
+	while (true)
+	{
+		Print(myQueue);
+
+		std::cout << "> ";
+		std::cin >> command;
+
+		switch (command)
+		{
+		case ENQUEUE:
+		{
+			int value;
+			std::cout << "\tvalue : ";
+			std::cin >> value;
+			EnQueue(myQueue, value);
+			break;
+		}
+		case DEQUEUE:
+			DeQueue(myQueue);
+			break;
+
+		case QUIT:
+			return 0;
+			break;
+
+		case DELETEALL:
+			DeleteAll(myQueue);
+			break;
+
+		default:
+			std::cout << "잘못된 명령어입니다" << std::endl;
+			break;
+		}
+	}
 }
--- DynamicQueue/queue.cpp
+++ DynamicQueue/queue.cpp
@@ -4,30 +4,63 @@
 void EnQueue(Queue& queue, int value)
 {
 	Element* pElement = new Element{};
-	pElement->value = value;	
+	pElement->value = value;
+	
 	
 	if (queue.count == 0)
 	{
-		queue.pHead = pElement;
-		queue.pTail = pElement;
+		queue.pHead = queue.pTail = pElement;
 	}
 
 	else if (queue.count > 0)
 	{
 		queue.pTail->pNext = pElement;
-		queue.pTail = pElement;
+		queue.pTail = queue.pTail->pNext;
 	}
 	queue.count++;
 }
 void DeQueue(Queue& queue)
 {
+	if(queue.count == 0)
+	{
+		std::cout << "QUEUE is already Empty" << std::endl;
+		return;
+	}
 
+	else if (queue.count > 0)
+	{
+		Element* pTemp = queue.pHead;
+		queue.pHead = queue.pHead->pNext;
+		pTemp->pNext = nullptr;
+		delete pTemp;			
+	}
+	queue.count--;
 }
 void Print(Queue& queue)
 {
-
+	if (queue.count == 0)
+	{
+		std::cout << "Empty" << std::endl;
+		return;
+	}
+	Element* pElement = queue.pHead;
+	while (pElement)
+	{
+		std::cout << pElement->value << " ";
+		pElement = pElement->pNext;
+	} std::cout << std::endl;
 }
 void DeleteAll(Queue& queue)
 {
+	Element* pElement = queue.pHead;
+	Element* pEmpty = nullptr;
 
+	while (pElement)
+	{
+		pEmpty = pElement;
+		pElement = nullptr;		
+		pEmpty = pEmpty->pNext;	
+		delete pElement;
+	}
+	queue.count = 0;
 }
